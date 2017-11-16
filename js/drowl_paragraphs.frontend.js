08be811e4ce67e5567caf4ee1b267c3d37dd865c
info@webks.de <info@webks.de>
2017-11-16 13:01:43 +0100

    Added theme preprocess for paragraph.

--- js/drowl_paragraphs.frontend.js
+++ js/drowl_paragraphs.frontend.js
@@ -1,14 +1,14 @@
 /**
 * @file
-* Placeholder file for custom sub-theme behaviors.
+* DROWL Paragraphs frontend JavaScript
 *
 */
 (function ($, Drupal) {
 
-  Drupal.behaviors.drowl_paragraphs_backend = {
+  Drupal.behaviors.drowl_paragraphs_frontend = {
     attach: function (context, settings) {
       $( document ).ready(function() {
-          console.log('paragraphs backend scripts!');
+          console.log('paragraphs frontend scripts!');
       });
     },
   }
