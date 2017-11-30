2dc4b450611b16a5b3b4342e66ee0f5441896264
info@webks.de <info@webks.de>
2017-11-30 11:29:56 +0100

    Split module into drowl_paragraphs and drowl_paragraphs_types

---
+++ modules/drowl_paragraphs_types/js/drowl_paragraphs_types.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * DROWL Paragraphs frontend JavaScript
+ *
+ */
+(function ($, Drupal) {
+  Drupal.behaviors.drowl_paragraphs_types = {
+    attach: function (context, settings) {
+      $(document).ready(function () {
+      });
+    }
+  }
+})(jQuery, Drupal);
