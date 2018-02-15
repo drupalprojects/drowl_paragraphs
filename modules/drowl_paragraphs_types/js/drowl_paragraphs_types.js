345fa45b76f42a2363df3d6daf72950817b036dc
info@webks.de <info@webks.de>
2018-02-15 13:21:26 +0100

    Fixed missing brackets in condition.

--- modules/drowl_paragraphs_types/js/drowl_paragraphs_types.js
+++ modules/drowl_paragraphs_types/js/drowl_paragraphs_types.js
@@ -3,11 +3,11 @@
  * DROWL Paragraphs frontend JavaScript
  *
  */
-(function ($, Drupal) {
-  Drupal.behaviors.drowl_paragraphs_types = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-      });
-    }
-  }
-})(jQuery, Drupal);
+// (function ($, Drupal) {
+//   Drupal.behaviors.drowl_paragraphs_types = {
+//     attach: function (context, settings) {
+//       $(document).ready(function () {
+//       });
+//     }
+//   }
+// })(jQuery, Drupal);
